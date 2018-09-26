57d8fc44e1454edb03212940f1412b812919b602
Gorrila <wrestbild57@gmail.com>
2018-09-26 05:56:48 +0300

    new chat

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const socketIO = require('socket.io');
+const path = require('path');
+const http = require('http');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000 ;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+
+server.listen(port, () => {
+  console.log(`Server has been started on port ${port}`)
+});
